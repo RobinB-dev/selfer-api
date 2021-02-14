a7f67e1a8dcb7be10ace354ee4fa0373d59ade93
Rob1 <60504992+RobinB-dev@users.noreply.github.com>
2021-02-15 00:03:53 +0100

    add infos3

--- server.js
+++ server.js
@@ -7,3 +7,12 @@ const port = process.env.PORT || 3000;
 server.use(middlewares);
 server.use(router);
 server.listen(port);
+
+
+// TO DO
+
+// git add .
+// git commit -m"add infos"
+// git push origin HEAD
+// git push heroku main
+// heroku open
